a690a8262d4d173763e8ac5a1ffa1c8057255768
venkatkumarp <venkat.linux2015@gmail.com>
2024-12-06 16:41:41 +0530

    Update locals.tf

--- platforms/aws/locals.tf
+++ platforms/aws/locals.tf
@@ -1,27 +1,30 @@
 locals {
 
-  project_name = "time-tracking"
+  project_name = "time-test"
 
   environment        = lookup(local.environments, var.aws_account_id, "none")
   environment_suffix = lookup(local.suffixes, var.aws_account_id, "")
 
   environments = {
     "440744244651" = "dev"
+    "423623838336" = "prod"
   }
 
   suffixes = {
     "440744244651" = "dev"
+    "423623838336" = "prod"
   }
 
   vpc_cidrs = {
     "440744244651" = "10.149.176.0/25"
+    "423623838336" = "10.149.177.0/25"
   }
 
   vpc_cidr = lookup(local.vpc_cidrs, var.aws_account_id, {})
 
   aws_region = "eu-central-1"
 
-  aws_infra_deploy_role = "arn:aws:iam::${var.aws_account_id}:role/infra-dev-deploy-roles"
+  aws_infra_deploy_role = "arn:aws:iam::${var.aws_account_id}:role/infra-${local.environment}-deploy-roles"
 
 
   network_config = {
