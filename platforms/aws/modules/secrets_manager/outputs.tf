2fdc8f7c3780dd81f26881e93bf9b790c45f9652
venkatkumarp <venkat.linux2015@gmail.com>
2024-12-09 12:49:52 +0530

    Update outputs.tf

--- platforms/aws/modules/secrets_manager/outputs.tf
+++ platforms/aws/modules/secrets_manager/outputs.tf
@@ -1,10 +1,7 @@
-#output "secret_values" {
-#  value = jsondecode(aws_secretsmanager_secret_version.secret_version.secret_string)
-#}
-#output "secret_arn" {
-#  description = "The ARN of the secret"
-# value       = aws_secretsmanager_secret.secret.arn
-#}
+output "secret_arn" {
+  description = "The ARN of the secret"
+ value       = aws_secretsmanager_secret.secret.arn
+}
 output "secret_values" {
   description = "The key-value pairs of secret values"
   value = jsondecode(aws_secretsmanager_secret_version.secret_version.secret_string)
