6857627d55a5a739a39651957d3a3c86353931bb
venkatkumarp <venkat.linux2015@gmail.com>
2024-12-09 13:46:46 +0530

    Update main.tf

--- platforms/aws/main.tf
+++ platforms/aws/main.tf
@@ -58,4 +58,5 @@ module "lambda_gettoken" {
   client_secret = module.secrets_manager.secret_values["client_secret"]
   code_verifier = module.secrets_manager.secret_values["code_verifier"]
   code_challenge = module.secrets_manager.secret_values["code_challenge"]
+  code_challenge_method = module.secrets_manager.secret_values["code_challenge_method"]
 }
