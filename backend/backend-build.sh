cc3c6da909392e698d630329a9b3e46685c37a9a
venkatkumarp <venkat.linux2015@gmail.com>
2024-12-14 13:14:25 +0530

    Update backend-build.sh

--- backend/backend-build.sh
+++ backend/backend-build.sh
@@ -68,7 +68,7 @@ fi
 
 # Create ZIP file for Lambda Function (Backend folder)
 # Exclude Terraform, sonar, and build script files
-if ! zip -r "$output_path_function" backend -x \*.tf \*sonar-project.properties \*backend-build.sh \*dev.tfbackend \*export-deps.sh \*api\* >&2; then
+if ! zip -r "$output_path_function" backend -x \*.tf \*sonar-project.properties \*backend-build.sh \*.tfbackend \*poetry.lock \*export-deps.sh \*api\* >&2; then
     error_exit "Failed to create Lambda Function ZIP file"
 fi
 
